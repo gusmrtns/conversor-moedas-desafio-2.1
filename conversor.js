59c23d8aaecf56240affb8fa712468f3e1c8bc9a
Gustavo Martins <gustavomartins9029@gmail.com>
2024-11-26 09:03:45 -0300

    feat: adicionada validações do conversor

--- conversor.js
+++ conversor.js
@@ -7,6 +7,14 @@ export default async function conversor() {
     let valor = await view.obterValor();
     let API_KEY = "ffb08fa4fc3d15150729cc7b";
 
+    if (!await validarDiferencaMoedas(moedaOrigem, moedaDestino)) {
+        return view.exibirMensagemDeErro("Moedas de origem e destino iguais.");
+    }
+
+    if (!await validarTamanhoMoeda(moedaOrigem) || !await validarTamanhoMoeda(moedaDestino)) {
+        return view.exibirMensagemDeErro("Moedas devem ter 3 caracteres.");
+    }
+
     fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/pair/${moedaOrigem}/${moedaDestino}/${valor}`, {
         method: 'GET',
     }).then(response => {
@@ -17,4 +25,18 @@ export default async function conversor() {
         view.exibirMensagemDeErro(error);
     })
 
+}
+
+async function validarDiferencaMoedas(moedaOrigem, moedaDestino) {
+    if (moedaOrigem === moedaDestino) {
+        return false;
+    }
+    return true;
+}
+
+async function validarTamanhoMoeda(moeda) {
+    if (moeda.length !== 3) {
+        return false;
+    }
+    return true;
 }
